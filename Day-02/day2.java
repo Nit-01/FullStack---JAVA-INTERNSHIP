ce71daf5d7d93497594fe56785a8ce69cb886671
Nit-01 <niteeshukotihal@gmail.com>
2026-02-21 00:42:40 +0530

    Day -02 Basics

---
+++ Day-02/day2.java
@@ -0,0 +1,15 @@
+public class day2 {
+    public static void main(String[] args) {
+
+        int age = 21;
+        double marks = 85.5;
+        char grade = 'A';
+        boolean result = true;
+
+        System.out.println(age);
+        System.out.println(marks);
+        System.out.println(grade);
+        System.out.println(result);
+
+    }
+}
